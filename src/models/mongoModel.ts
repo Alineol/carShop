0bf4fdbeade0dda5563e84906af575de1ec64ce9
Aline <allinne.oliveira.ol@gmail.com>
2022-08-16 16:39:59 -0300

    feat: cria model mngo

---
+++ src/models/mongoModel.ts
@@ -0,0 +1,35 @@
+import { Model, isValidObjectId } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
+import { ErrorTypes } from '../errors/errosCatalog';
+
+abstract class MongoModel<T> implements IModel<T> {
+  protected _model: Model<T>;
+
+  constructor(model:Model<T>) {
+    this._model = model;
+  }
+  public async create(object: T): Promise<T> {
+    return this._model.create({ ...object });
+  }
+
+  public async readOne(_id:string):Promise<T | null> {
+    if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
+    return this._model.findOne({ _id });
+  }
+
+  public async read(): Promise<T[]> {
+    return this._model.find();
+  }
+
+  public async update(_id: string, object: T): Promise<T | null> {
+    await this._model.updateOne({ _id }, object);
+    return object;
+  }
+
+  public async delete(_id: string): Promise<T | null> {
+    await this._model.deleteOne({ _id });
+    return null;
+  }
+}
+
+export default MongoModel;
