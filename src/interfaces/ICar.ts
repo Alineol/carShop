f7cb616f875fb252512b36c3ddd05f71b95b7e69
Aline <allinne.oliveira.ol@gmail.com>
2022-08-16 15:59:42 -0300

    feat: cria interface ICar

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+import { vehicleZodShema } from './IVehicle';
+
+const carSchema = vehicleZodShema.extend({
+  doorsQty: z.number().min(2).max(4),
+  seatsQty: z.number().max(7).min(2),
+});
+
+export type ICar = z.infer<typeof carSchema>;
--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -9,3 +9,5 @@ const vehicleZodShema = z.object({
 });
 
 export type IVehicle = z.infer<typeof vehicleZodShema>;
+
+export { vehicleZodShema };
