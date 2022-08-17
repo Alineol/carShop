168a238e99dadd13756c095f7a0c4af08751e5fc
Aline <allinne.oliveira.ol@gmail.com>
2022-08-17 12:50:01 -0300

    feat: metodo readOne car na camada service

--- src/services/carService.ts
+++ src/services/carService.ts
@@ -1,7 +1,8 @@
+import { isValidObjectId } from 'mongoose';
 import IService from '../interfaces/IService';
 import { ICar, carSchema } from '../interfaces/ICar';
 import { IModel } from '../interfaces/IModel';
-// import { ErrorTypes } from '../errors/errosCatalog';
+import { ErrorTypes } from '../errors/errosCatalog';
 
 class CarService implements IService<ICar> {
   private _car: IModel<ICar>;
@@ -20,6 +21,17 @@ class CarService implements IService<ICar> {
   public async read(): Promise<ICar[] > {
     const cars = await this._car.read();
     return cars;
+  }
+
+  public async readOne(_id: string): Promise<ICar> {
+    if (!isValidObjectId(_id)) {
+      throw new Error(ErrorTypes.InvalidMongoId);
+    }
+    const car = await this._car.readOne(_id);
+    if (!car) {
+      throw new Error(ErrorTypes.EntityNotFound);
+    }
+    return car;
   }
 }
 
