ae915ee3f55cd0ec967215f170d192043ebca367
Aline <allinne.oliveira.ol@gmail.com>
2022-08-17 11:16:10 -0300

    test: cria mock para array de cars

--- src/tests/mocks/carMocks.ts
+++ src/tests/mocks/carMocks.ts
@@ -18,4 +18,23 @@ const carMockWithouId = {
   doorsQty: 2
 }
 
-export { carWithIdMock, carMockWithouId }
+const carsWithIdArray = [{
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2,
+  _id: 'djhdojdlkdjladjadfjapod'
+},
+{
+  model: "fiat",
+  year: 1963,
+  color: "blue",
+  buyValue: 350000,
+  seatsQty: 4,
+  doorsQty: 4,
+  _id: 'djhdojdlkduykyqahyfjapod'
+}]
+
+export { carWithIdMock, carMockWithouId, carsWithIdArray }
